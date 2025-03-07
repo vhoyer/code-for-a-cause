cf842d0e77da3c04f0a34f76ce613ed2c0b70dbf
Pemmy0 <pemmylicious00@gmail.com>
2025-03-08 06:30:53 +0700

    Added Pause Menu

--- modules/UI/ui_elements/volume_slider/h_volume_slider.gd
+++ modules/UI/ui_elements/volume_slider/h_volume_slider.gd
@@ -1,5 +1,5 @@
 @tool
-extends VBoxContainer
+extends HSlider
 class_name HVolumeSlider
 
 @export
@@ -10,9 +10,6 @@ var bus: StringName:
 
 
 var bus_id
-var label: Label
-var h_slider: HSlider
-
 
 signal _internal_update()
 
@@ -21,20 +18,10 @@ func _ready() -> void:
 	self.custom_minimum_size.x = 150.0
 	bus_id = AudioServer.get_bus_index(bus)
 
-	for child in self.get_children():
-		self.remove_child(child)
-
-	label = Label.new()
-	self.add_child(label)
-	_internal_update.connect(func():
-		label.text = bus)
-
-	h_slider = HSlider.new()
-	self.add_child(h_slider)
-	h_slider.max_value = 1.0
-	h_slider.step = 0.05
-	h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_id))
-	h_slider.value_changed.connect(_on_h_slider_value_changed)
+	self.max_value = 1.0
+	self.step = 0.05
+	self.value = db_to_linear(AudioServer.get_bus_volume_db(bus_id))
+	self.value_changed.connect(_on_h_slider_value_changed)
 
 	_internal_update.emit()
 

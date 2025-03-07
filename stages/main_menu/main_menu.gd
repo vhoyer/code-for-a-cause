0dabad86c9dda2d7a84f60aa1235d570d8f918a9
Vinícius Hoyer <contact@vhoyer.dev>
2025-03-07 13:31:23 -0300

    ♻️ Make controller focus start feature work based on nodes

--- stages/main_menu/main_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if not get_viewport().gui_get_focus_owner():
-		if Input.get_connected_joypads().size() > 0:
-			%StartGame.grab_focus()

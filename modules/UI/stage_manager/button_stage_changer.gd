ede8d04de2a344da9bc3cfcc37dadc597bf3bbe7
Vinícius Hoyer <contact@vhoyer.dev>
2025-03-07 15:30:23 -0300

    ✨ Create a choose level scene

--- modules/UI/stage_manager/button_stage_changer.gd
+++ modules/UI/stage_manager/button_stage_changer.gd
@@ -1,11 +1,15 @@
 extends Button
 class_name ButtonSceneChanger
 
-@export var scene: PackedScene
-
 ## If go_back is set to true, scene variable will be ignored
-@export var go_back: bool = false
-@export var go_back_by: int = 1
+@export_enum('to scene', 'back by count', 'back to start')
+var method: String = 'to scene'
+
+@export
+var go_back_by_count: int = 1
+
+@export
+var scene: PackedScene
 
 @export var payloads: Dictionary
 
@@ -17,14 +21,19 @@ func _ready() -> void:
 	connect("button_up", _button_up)
 
 func _button_up() -> void:
-	if has_scene_manager:
-		var mngr = StageManager.singleton
-		if go_back:
-			StageManager.go_back(go_back_by)
-		else:
-			for key in payloads.keys():
-				StageManager.set_payload(key, payloads[key])
-			mngr.change_stage_to_packed(scene)
-	else:
+	if not has_scene_manager:
 		var err = get_tree().change_scene_to_packed(scene)
 		if err != OK: print(error_string(err))
+		return
+
+	get_tree().paused = false
+	
+	match method:
+		'to scene':
+			for key in payloads.keys():
+				StageManager.set_payload(key, payloads[key])
+			StageManager.push_stage(scene)
+		'back to start':
+			StageManager.go_to_start()
+		'back by count':
+			StageManager.go_back(go_back_by_count)
---
+++ stages/level_select/level_list.gd
@@ -0,0 +1,34 @@
+@tool
+extends GridContainer
+
+@export_dir var level_dir;
+
+@export_tool_button('Populate Level List')
+var populate_btn = populate_grid
+
+func populate_grid() -> void:
+	for child in self.get_children():
+		self.remove_child(child)
+
+	var dir = DirAccess.open(level_dir)
+	if !dir:
+		printerr("The gods have failed us, no dir '%s' found" % [level_dir]);
+		pass
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "": break
+		if !dir.current_is_dir(): continue
+		var path = "%s/%s/%s.tscn" % [dir.get_current_dir(true), file_name, file_name]
+		if not FileAccess.file_exists(path): continue
+		add_game(path, file_name)
+	dir.list_dir_end()
+	pass
+
+func add_game(path: String, title: String):
+	var btn = ButtonSceneChanger.new()
+	btn.name = title.replace('_', ' ')
+	self.add_child(btn, true)
+	btn.scene = load(path)
+	btn.text = btn.name
+	btn.owner = self.owner;

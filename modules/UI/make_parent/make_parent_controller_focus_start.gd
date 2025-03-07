b378646a3354e16554970a77ffd6a5dcee225eb4
Vinícius Hoyer <contact@vhoyer.dev>
2025-03-07 13:40:50 -0300

    🚸 Make pause menu have also the controller focus

--- modules/UI/make_parent/make_parent_controller_focus_start.gd
+++ modules/UI/make_parent/make_parent_controller_focus_start.gd
@@ -2,6 +2,7 @@ extends Node
 class_name MakeParentControllerFocusStart
 
 func _process(_delta: float) -> void:
-	if not get_viewport().gui_get_focus_owner():
+	var focused_control = get_viewport().gui_get_focus_owner()
+	if not focused_control:
 		if Input.get_connected_joypads().size() > 0:
 			self.get_parent().grab_focus()

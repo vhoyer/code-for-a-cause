2df7f7be2e46ff32d0d47416fba451fc188b830a
Vinícius Hoyer <contact@vhoyer.dev>
2025-03-07 22:27:08 -0300

    ✨ Adjusting camera on door open

--- entities/camera/camera.gd
+++ entities/camera/camera.gd
@@ -11,7 +11,7 @@ static var singleton: CameraMain:
 @export var zoom_constant: float = 0.8
 
 var main_poi: Vector2
-var poi_list: Array[Node2D] = []
+var poi_list: Array[Vector2] = []
 
 
 func _init() -> void:
@@ -21,22 +21,24 @@ func _exit_tree() -> void:
 	singleton = null
 
 
-func momentarily_add_to_focus(node: Node2D) -> void:
+func momentarily_add_to_focus(point: Vector2) -> void:
 	var tween = create_tween()
 	tween.tween_callback(func():
-		poi_list.push_back(node))
+		poi_list.push_back(point))
 	tween.tween_interval(3)
 	tween.tween_callback(func():
-		poi_list.erase(node))
+		poi_list.erase(point))
 
 
 func get_rect_containing_points_of_interest() -> Rect2:
 	var list = poi_list.duplicate()
 	var rect:= Rect2(main_poi, Vector2.ONE)
 
-	for node: Node2D in list:
-		rect = rect.expand(node.global_position)
+	for point: Vector2 in list:
+		rect = rect.expand(point)
 
+	rect.position.y -= 35
+	rect.size.y += 70
 	return rect
 
 
@@ -54,7 +56,7 @@ func _physics_process(_delta: float) -> void:
 	if poi_list.size():
 		var zoom_factor_x = get_viewport_rect().size.x / rect.size.x * zoom_constant
 		var zoom_factor_y = get_viewport_rect().size.y / rect.size.y * zoom_constant
-		var zoom_factor = clamp(min(zoom_factor_x, zoom_factor_y), 0.1, 1)
+		var zoom_factor = clamp(min(zoom_factor_x, zoom_factor_y), 0.01, 1)
 		self.zoom = Vector2(zoom_factor, zoom_factor)
 	else:
 		self.zoom = Vector2.ONE
--- entities/door/open_door.gd
+++ entities/door/open_door.gd
@@ -9,4 +9,6 @@ func _ready() -> void:
 
 
 func _on_active_changed(active: bool) -> void:
+	CameraMain.singleton.momentarily_add_to_focus(door.global_position + Vector2(0, 50))
+	await get_tree().create_timer(1).timeout
 	door.is_open = active

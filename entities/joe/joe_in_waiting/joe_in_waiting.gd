ac2c64980ca0cc6ad410d9e6c5d8fb2677b28680
Vinícius Hoyer <contact@vhoyer.dev>
2025-03-08 00:08:10 -0300

    ✨ Joe in waiting done

--- entities/joe/joe_in_waiting/joe_in_waiting.gd
+++ entities/joe/joe_in_waiting/joe_in_waiting.gd
@@ -1,26 +1,24 @@
 extends Area2D
 class_name JoeInWaiting
 
-signal active_changed(active: bool)
+@export
+var player: PlayerController
 
-var active: bool = false:
-	set(value):
-		if active == value: return
-		active = value
-		active_changed.emit(active)
+var activated: bool = false
 
-
-func _ready() -> void:
-	active_changed.connect(update_view)
-
-
-func update_view() -> void:
-	self.visible = !active
+func add_a_joe() -> void:
+	self.visible = !activated
+	if activated:
+		player.add_a_joe()
 
 
 func _on_body_entered_exited(body: Node2D) -> void:
+	if activated: return
+
 	var body_list = self.get_overlapping_bodies()
 	var is_a_joe = body_list.any(func(body: Node2D):
 		return body is Joe)
+
 	if is_a_joe:
-		active = true
+		activated = true
+		add_a_joe()

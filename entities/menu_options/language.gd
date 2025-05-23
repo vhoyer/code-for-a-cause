f78620fcd945b440bfd5a20cf9119409a765fe96
Vinícius Hoyer <contact@vhoyer.dev>
2025-05-23 17:27:19 -0300

    ✨ Auto load language setting on game execution

--- entities/menu_options/language.gd
+++ entities/menu_options/language.gd
@@ -4,10 +4,9 @@ extends HWheelPickerButton
 func _ready() -> void:
 	self.clear_items()
 	for locale in TranslationServer.get_loaded_locales():
-		print(locale)
 		self.add_item(locale, { 'locale': locale })
 		
 
 func _item_selected(index: int) -> void:
 	var locale = self.get_item_metadata(index).get('locale', 'en')
-	TranslationServer.set_locale(locale)
+	SettingsManager.set_language(locale)

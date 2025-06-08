02b912fae137cc110c6b62561c280fdbf89d28e4
Vinícius Hoyer <contact@vhoyer.dev>
2025-06-07 21:20:34 -0300

    🌐 Add tr() to user facing strings on scripts to allow for POT generation

--- entities/menu_options/fullscreen_on_off.gd
+++ entities/menu_options/fullscreen_on_off.gd
@@ -4,8 +4,8 @@ extends HWheelPickerButton
 
 func _ready() -> void:
 	self.clear_items()
-	self.add_item('On', DisplayServer.WINDOW_MODE_FULLSCREEN)
-	self.add_item('Off', DisplayServer.WINDOW_MODE_WINDOWED)
+	self.add_item(tr('On', &"true false picker"), DisplayServer.WINDOW_MODE_FULLSCREEN)
+	self.add_item(tr('Off', &"true false picker"), DisplayServer.WINDOW_MODE_WINDOWED)
 	if Engine.is_editor_hint(): return
 	self.selected = self.get_index_by_metadata(SettingsManager.get_fullscreen())
 

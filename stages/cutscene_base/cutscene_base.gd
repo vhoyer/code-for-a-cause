623eb0fb0fc3370ac9773f2a1059a8f1e14ee5ab
Vinícius Hoyer <contact@vhoyer.dev>
2025-06-11 20:58:55 -0300

    ✨ Change how you go to frame positions on cutscene levels

--- stages/cutscene_base/cutscene_base.gd
+++ stages/cutscene_base/cutscene_base.gd
@@ -17,9 +17,10 @@ func _ready() -> void:
 	DialogueManager.show_dialogue_balloon(dialogue)
 
 
-func next_frame(index:= 0) -> void:
-	var marker: Marker2D = positions.get_child(index)
+func go_to(name: String) -> void:
+	var marker: Marker2D = positions.get_node(NodePath(name))
 	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
 	tween.tween_property(camera, 'global_position', marker.global_position, 1)
 	await tween.finished
 
